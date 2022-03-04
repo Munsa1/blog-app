d6f1cf7f3e351bb63cd9ff888e454b16ce2bed15
Munsa1 <munsamibenge65@gmail.com>
2022-03-04 23:03:16 +0200

    Update posts controller with logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,18 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
+    @current_user = current_user
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+
+    if @post.save 
+      flash[:success] = 'Post was saved'
+      redirect_to "/users/#{@post.user.id}/posts/"
+    else
+      render :new
+    end
   end
 end
