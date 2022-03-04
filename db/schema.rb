d0a6ab1fc0a5990429e81a2a196144e5e77cc09d
Munsa1 <munsamibenge65@gmail.com>
2022-03-04 13:46:02 +0200

    Fix seed file errors

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_25_191237) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "Text"
+    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
@@ -34,12 +34,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_25_191237) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "Title"
-    t.string "Text"
-    t.date "CreatedAt"
-    t.date "UpdatedAt"
-    t.integer "CommentsCounter"
-    t.integer "LikesCounter"
+    t.string "title"
+    t.string "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
@@ -47,10 +45,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_25_191237) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "Name"
-    t.string "Photo"
-    t.text "Bio"
-    t.integer "PostsCounter"
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+bio
+user1 = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F-0BxGuVvo', bio: 'Teacher from Mexico.')
+user2 = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F-0BxGuVvo', bio: 'Teacher from Poland.')
+user3 = User.create(name: 'Anna', photo: 'https://unsplash.com/photos/F-0BxGuVvo', bio: 'Teacher from Poland.')
 
-user1 = User.create(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.')
-user2 = User.create(Name: 'Lilly', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Poland.')
-user3 = User.create(Name: 'Anna', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Poland.')
+post1 = Post.create(author: User.first, title: 'Hello', text: 'This is my first post')
+post1 = Post.create(author: User.second, title: 'Hello', text: 'This is my second post')
+post1 = Post.create(author: User.third, title: 'Hello', text: 'This is my third post')
+post1 = Post.create(author: User.first, title: 'Hello all', text: 'This is my fifth post')
 
-post1 = Post.create(author: User.first, Title: 'Hello', Text: 'This is my first post')
-post1 = Post.create(author: User.second, Title: 'Hello', Text: 'This is my second post')
-post1 = Post.create(author: User.third, Title: 'Hello', Text: 'This is my third post')
-post1 = Post.create(author: User.first, Title: 'Hello all', Text: 'This is my fifth post')
-
-comment1 = Comment.create(post: Post.first,author: User.first, Text: 'Hi Tom!' )
-comment2 = Comment.create(post: Post.first,author: User.first, Text: 'Hi Tom!' )
-comment3 = Comment.create(post: Post.second,author: User.second, Text: 'Hi Lilly!' )
-comment4 = Comment.create(post: Post.second,author: User.second, Text: 'Hi Lilly!' )
-comment5 = Comment.create(post: Post.third,author: User.third, Text: 'Hi Anna!' )
-comment5= Comment.create(post: Post.third,author: User.third, Text: 'Hi Anna!' )
+comment1 = Comment.create(post: Post.first,author: User.first, text: 'Hi Tom!' )
+comment2 = Comment.create(post: Post.first,author: User.first, text: 'Hi Tom!' )
+comment3 = Comment.create(post: Post.second,author: User.second, text: 'Hi Lilly!' )
+comment4 = Comment.create(post: Post.second,author: User.second, text: 'Hi Lilly!' )
+comment5 = Comment.create(post: Post.third,author: User.third, text: 'Hi Anna!' )
+comment5= Comment.create(post: Post.third,author: User.third, text: 'Hi Anna!' )
