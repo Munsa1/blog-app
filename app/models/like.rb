b5c3c65e5def45afff7e24d9946a267885d67f1a
Munsa1 <munsamibenge65@gmail.com>
2022-03-05 22:10:04 +0200

    Make changes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ActiveRecord::Base
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   belongs_to :user, class_name: 'User', foreign_key: :user_id
   belongs_to :post
 
--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_25_191237) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "photo"
     t.text "bio"
     t.integer "posts_counter"
